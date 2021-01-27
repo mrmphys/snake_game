a575c430f209f8f66b0b73e86ecd09a53a482716
MRM <githubmails@gmail.com>
2021-01-26 23:02:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from playsnake import PlaySnake
 from turtle import Screen
-start_screen = Screen()
-user_bet = start_screen.textinput(title="Start the Game",prompt='')
 play = PlaySnake()
 play.play_start()
